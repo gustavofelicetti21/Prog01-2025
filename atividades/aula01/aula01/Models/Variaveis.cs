993fdc5146fe2c397e45eadb43b180612c114a97
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2025-02-24 22:02:02 -0300

    atividade 1

---
+++ atividades/aula01/aula01/Models/Variaveis.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Formats.Asn1;
+
+public class Variaveis
+{
+    //Anotações de tipos
+    public int userCount;
+
+    //Uma variavel pode ser declarada
+    //e nao inicializada
+    public int totalCount;
+
+    // CONSTANTES
+    // para declarar uma constante
+    // utilizamos a palavra-chave CONST
+    // No entanto a CONST deve ser inicializada
+    // quando declarada
+    const int interestRate = 10;
+
+    //o metodo construtor e invocado
+    //na inicialização do objeto por meio
+    // da palavra reservada new
+    // Por regra, o construtor sempre tem
+    // o mesmo nome da classe
+    public Variaveis()
+	{
+        totalCount = 0;
+
+        //tipo implícito
+        // a palavvra chave var se encarrega
+        // de definir o tipo da variável
+        // na instrução de atribuição
+        var signalStrength = 22;
+        var companyName = "ACME";
+	}
+}
--- atividades/aula01/aula01/obj/Debug/net9.0/aula01.AssemblyInfo.cs
+++ atividades/aula01/aula01/obj/Debug/net9.0/aula01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae82e8e96a72c952b6f5b3eb5fbbc75b180f4d4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
