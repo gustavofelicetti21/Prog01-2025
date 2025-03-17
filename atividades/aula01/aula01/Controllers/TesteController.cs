2cdd86120fa2b6be09e20b970822177da0f47076
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2025-03-17 12:12:04 -0300

    atividade 2 e 1

---
+++ atividades/aula01/aula01/Controllers/TesteController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace aula01.Controllers
+{
+    public class Result
+    {
+        public string Texto = string.Empty;
+    }
+    public class TesteController : Controller
+    {
+        private readonly ILogger<TesteController> _logger;
+
+        public TesteController(
+            ILogger<TesteController> logger
+            )
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index", new Result());
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string texto)
+        {
+            Result resultado = new Result();
+
+            int criptografia = Convert.ToInt32(texto) + 5;
+            int letra
+            if (int letra > 122) { letra = letra - 26; }
+            //converte ascii para char 
+            criptografia += Convert.ToChar(letra);
+        }
+
+            return View("Index", resultado);
+        }
+    }
+}
