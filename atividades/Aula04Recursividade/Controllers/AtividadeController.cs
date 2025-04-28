3890930a107121e5a813b06823392349b213768f
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2025-04-28 19:39:04 -0300

    atividade 4 feita pela metade

---
+++ atividades/Aula04Recursividade/Controllers/AtividadeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula04Recursividade.Controllers
+{
+    public class AtividadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+
+
+    }
+}
